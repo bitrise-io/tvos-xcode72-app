fcbd2bf627b69014433b74f9813da1aeb41cbae6
Viktor Benei <viktor.benei@gmail.com>
2015-12-30 08:54:11 +0100

    base project

---
+++ tvos-xcode72-app/tvos-xcode72-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tvos-xcode72-app
+//
+//  Created by Viktor Benei on 30/12/15.
+//  Copyright © 2015 bitrise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ tvos-xcode72-app/tvos-xcode72-appTests/tvos_xcode72_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  tvos_xcode72_appTests.swift
+//  tvos-xcode72-appTests
+//
+//  Created by Viktor Benei on 30/12/15.
+//  Copyright © 2015 bitrise. All rights reserved.
+//
+
+import XCTest
+@testable import tvos_xcode72_app
+
+class tvos_xcode72_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
